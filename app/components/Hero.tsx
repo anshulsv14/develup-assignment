f29404649e9edbd8d022abdb666b4458d5e98b98
Anshul shivhare <anshulsv14@gmail.com>
2025-07-17 08:35:24 +0530

    complete

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -6,14 +6,14 @@ import SearchBar from "./SearchBar";
 
 const Hero = () => {
   return (
-    <div className="min-h-[500px] bg-gradient-to-r  from-sky-100 to-white-600 pt-[4rem] m-auto ">
-      <div className="max-w-7xl grid grid-cols-2  gap-4 m-auto py-10 ">
+    <div className="min-h-[500px] bg-gradient-to-r from-sky-100 to-white-600 pt-16 m-auto">
+      <div className="max-w-7xl grid grid-cols-1 lg:grid-cols-2 gap-4 m-auto py-10 px-4">
+        
         <div className="flex items-center">
-          {" "}
           <div className="flex flex-col gap-6">
             <HeroTab />
             <Heading
-              className="!text-8xl leading-[90px] text-left !mb-0"
+              className="text-4xl sm:text-5xl md:text-6xl lg:!text-8xl leading-tight lg:leading-[90px] text-left !mb-0"
               heading={[
                 { text: "Learn.", color: "text-black" },
                 { text: " Earn. ", color: "text-blue-600" },
@@ -21,30 +21,29 @@ const Hero = () => {
                 { text: "Up.", color: "text-blue-700" },
               ]}
             />
-            <p className="text-[20px] leading-[30px] text-gray-600 font-sora font-normal ">
+            <p className="text-base sm:text-lg md:text-xl leading-[30px] text-gray-600 font-sora font-normal">
               Don't chase jobs! Build skills, get hired.
             </p>
             <SearchBar />
           </div>
         </div>
-        <div className="flex justify-end relative ">
+
+       
+        <div className="hidden lg:flex justify-end relative">
           <img
             className="h-[450px]"
             src="/assets/Hero/heroimage.png"
-            
-            alt=""
+            alt="hero"
           />
           <img
-            className="absolute top-15 left-40 h-[80px] w-auto"
+            className="absolute top-16 left-40 h-[80px] w-auto"
             src="/assets/Hero/hero-card-1.png"
-           
-            alt=""
+            alt="card1"
           />
           <img
-            className="absolute bottom-10 left-50 h-[90px] w-auto"
+            className="absolute bottom-10 left-52 h-[90px] w-auto"
             src="/assets/Hero/hero-card-2.png"
-            
-            alt=""
+            alt="card2"
           />
         </div>
       </div>
@@ -53,3 +52,4 @@ const Hero = () => {
 };
 
 export default Hero;
+
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import ResumeBuilderBanner from "./components/ResumeBuilderBanner";
 import Features from "./components/Features";
 import ResumeSection from "./components/ResumeSection";
 import Footer from "./components/Footer";
-import Recruitment from "./components/Banner";
+
 
 import Banner from "./components/Banner";
 import Navbar from "./components/Navbar";
